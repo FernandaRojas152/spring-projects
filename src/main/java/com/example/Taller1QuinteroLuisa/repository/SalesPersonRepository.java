26c774ad62c2ddd6a0f2ea6991cfb31df793ff8f
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-03-14 17:12:09 -0500

    salesperson service added

--- src/main/java/com/example/Taller1QuinteroLuisa/repository/SalesPersonRepository.java
+++ src/main/java/com/example/Taller1QuinteroLuisa/repository/SalesPersonRepository.java
@@ -1,8 +1,12 @@
 package com.example.Taller1QuinteroLuisa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.Taller1QuinteroLuisa.model.sales.Salesperson;
 
-public interface SalesPersonRepository extends CrudRepository<Salesperson, Integer>{
+@Repository
+public interface SalesPersonRepository extends JpaRepository<Salesperson, Integer>{
 
 }
--- src/main/java/com/example/Taller1QuinteroLuisa/services/SalesPersonServiceImp.java
+++ src/main/java/com/example/Taller1QuinteroLuisa/services/SalesPersonServiceImp.java
@@ -1,20 +1,39 @@
 package com.example.Taller1QuinteroLuisa.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.Taller1QuinteroLuisa.model.sales.Salesperson;
 import com.example.Taller1QuinteroLuisa.repository.SalesPersonRepository;
 
+@Service
 public class SalesPersonServiceImp implements SalesPersonService{
 	
 	private SalesPersonRepository sp;
+	
+	@Autowired
 	public SalesPersonServiceImp(SalesPersonRepository sp) {
 		this.sp= sp;
 	}
 	
-	public void save() {
-		sp.save(new Salesperson());
+	public void save(Salesperson s) {
+		sp.save(s);
 	}
 	
-	public void edit() {
+	public void update(Salesperson s) {
+		Salesperson p= sp.getById(s.getBusinessentityid());
+		p.setModifieddate(s.getModifieddate());
+		p.setSaleslastyear(s.getSaleslastyear());
+		p.setBonus(s.getBonus());
+		p.setCommissionpct(s.getCommissionpct());
+		p.setRowguid(s.getRowguid());
+		p.setSalesquota(s.getSalesquota());
+		p.setSalesytd(s.getSalesytd());
 		
+		sp.save(p);
+	}
+	
+	public void delete(Salesperson s) {
+		sp.delete(s);
 	}
 }
