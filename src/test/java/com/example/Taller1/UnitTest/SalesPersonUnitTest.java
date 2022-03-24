a1a7da5a8b8cd8c66d298e4079375a431fb79a1f
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-03-23 23:38:54 -0500

    Another test added :+1:

--- src/test/java/com/example/Taller1/UnitTest/SalesPersonUnitTest.java
+++ src/test/java/com/example/Taller1/UnitTest/SalesPersonUnitTest.java
@@ -40,124 +40,148 @@ import com.example.Taller1QuinteroLuisa.services.SalesPersonServiceImp;
 //@SpringBootTest()
 class SalesPersonUnitTest {
 	private static final Integer BUSINESSENTITY_ID = 1522215;
-	
+
 	private Salesperson person;
 	private Employee employee;
 	//private Salesterritory territory;
-	
+
 	@Mock
 	private SalesPersonRepository salesPersonRepository;
-	
+
 	@Mock
 	private EmployeeRepository employeeRepository;
-	
+
 	@Mock
 	private SalesTerritoryRepository salesTerritoryRepository;
-	
+
 	@InjectMocks
 	private SalesPersonServiceImp salesPersonServiceImp;
-	
+
 	@BeforeAll
-    static void init() {
-        System.out.println("--------------- SALESPERSON TESTING -----------------");
-    }
-	
+	static void init() {
+		System.out.println("--------------- SALESPERSON TESTING -----------------");
+	}
+
 	@Nested
 	@DisplayName("Save methods for sales person")
 	class savePerson {
-		
+
 		@Test
 		void saveSalesPersonCorrect() throws Exception {
 			//Set up
-			
+
 			person= new Salesperson();
 			employee= new Employee();
-			
+
 			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-	        Date date  = df.parse("10-11-2022");
-	        long time1 = date.getTime();
-	        Timestamp time = new Timestamp(time1);
-	        person.setModifieddate(time);
-	        person.setSalesquota(new BigDecimal(152));
-	        person.setCommissionpct(BigDecimal.ZERO);
-	        person.setBonus(BigDecimal.ONE);
-	        
-	        Salesterritory territory= new Salesterritory();
-	        
+			Date date  = df.parse("10-11-2022");
+			long time1 = date.getTime();
+			Timestamp time = new Timestamp(time1);
+			person.setModifieddate(time);
+			person.setSalesquota(new BigDecimal(152));
+			person.setCommissionpct(BigDecimal.ZERO);
+			person.setBonus(BigDecimal.ONE);
+
+			Salesterritory territory= new Salesterritory();
+
 			//when(employeeRepository.findById(BUSINESSENTITY_ID)).thenReturn(Optional.of(employee));
 			when(salesTerritoryRepository.findById(57)).thenReturn(Optional.of(territory));
 			when(salesPersonRepository.save(person)).thenReturn(person);
-			
+
 			//Method
-			
+
 			Salesperson temp= salesPersonServiceImp.save(person,57);
-			
+
 			assertNotNull(temp);
 			assertEquals(new BigDecimal(152), temp.getSalesquota());
 			assertEquals(person.getBusinessentityid(), temp.getBusinessentityid());
 			assertEquals(new BigDecimal(0), temp.getCommissionpct());
 			assertEquals(territory, temp.getSalesterritory());
-			
+
 			verify(salesTerritoryRepository).findById(57);
 			//verify(employeeRepository).findById(BUSINESSENTITY_ID);
 			verify(salesPersonRepository).save(person);
 		}
-		
+
 		@Test
 		void salesPersonWrongSalesQuota() throws Exception {
 			person= new Salesperson();
 			employee= new Employee();
-			
+
 			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-	        Date date  = df.parse("10-11-2022");
-	        long time1 = date.getTime();
-	        Timestamp time = new Timestamp(time1);
-	        person.setModifieddate(time);
-	        try {
-	        	person.setSalesquota(new BigDecimal(-152));
-			} catch (Exception e) {
+			Date date  = df.parse("10-11-2022");
+			long time1 = date.getTime();
+			Timestamp time = new Timestamp(time1);
+			person.setModifieddate(time);
+			try {
+				person.setSalesquota(new BigDecimal(-152));
+			} catch (RuntimeException e) {
 				Throwable exception = assertThrows(RuntimeException.class, () -> person.getSalesquota());
-		        assertEquals("La cuota no es mayor que 0", exception.getMessage());
+				assertEquals("La cuota no es mayor que 0", exception.getMessage());
 			}
-	        
-	        person.setCommissionpct(BigDecimal.ZERO);
-	        person.setBonus(BigDecimal.ONE);
-	        
-	        //Salesperson temp= salesPersonServiceImp.save(person,57);
-	        
-	        //assertNull(temp);
-	        
-	        
-	        
+
+			person.setCommissionpct(BigDecimal.ZERO);
+			person.setBonus(BigDecimal.ONE);
+
+			//Salesperson temp= salesPersonServiceImp.save(person,57);
+			//assertNull(temp);
 		}
-		
+
 		@Test
 		void salesPersonWrongSalesQuotaNull() throws Exception {
 			person= new Salesperson();
 			employee= new Employee();
-			
+
 			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-	        Date date  = df.parse("10-11-2022");
-	        long time1 = date.getTime();
-	        Timestamp time = new Timestamp(time1);
-	        person.setModifieddate(time);
-	        person.setSalesquota(null);
-	        person.setCommissionpct(BigDecimal.ZERO);
-	        person.setBonus(BigDecimal.ONE);
-	        
-	        assertNull(person.getSalesquota());
-	        
-	        verify(salesPersonRepository, times(0)).save(person);
+			Date date  = df.parse("10-11-2022");
+			long time1 = date.getTime();
+			Timestamp time = new Timestamp(time1);
+			person.setModifieddate(time);
+			person.setSalesquota(null);
+			person.setCommissionpct(BigDecimal.ZERO);
+			person.setBonus(BigDecimal.ONE);
+
+			assertNull(person.getSalesquota());
+
+			verify(salesPersonRepository, times(0)).save(person);
 		}
-		
+
+		@Test
+		void salesPersonPercentageWrong() throws Exception {
+			person= new Salesperson();
+			employee= new Employee();
+
+			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+			Date date  = df.parse("10-11-2022");
+			long time1 = date.getTime();
+			Timestamp time = new Timestamp(time1);
+			person.setModifieddate(time);
+			person.setSalesquota(new BigDecimal(152));
+			try {
+				person.setCommissionpct(new BigDecimal(80));
+			}catch (RuntimeException e) {
+				Throwable exception = assertThrows(RuntimeException.class, () -> person.getSalesquota());
+				assertEquals("El porcentaje de comision no esta entre 0 y 1", exception.getMessage());
+			}
+			person.setBonus(BigDecimal.ONE);
+			
+			Salesterritory territory= new Salesterritory();
+			Salesperson temp= salesPersonServiceImp.save(person,57);
+			
+			assertNull(temp);
+			verify(salesPersonRepository, times(0)).save(person);
+			//verify(salesTerritoryRepository, times(0)).findById(57);
+
+		}
+
 	}
-	
+
 	/**@BeforeEach
 	void setUp() {
 		try {
 			salesPersonServiceImp= new SalesPersonServiceImp(salesPersonRepository, salesTerritoryRepository, employeeRepository);
 			person = new Salesperson();
-			
+
 			person.setBusinessentityid(BUSINESSENTITY_ID);
 			person.setSalesquota(new BigDecimal(152));
 			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
@@ -173,9 +197,9 @@ class SalesPersonUnitTest {
 			e.printStackTrace();
 		}
 	}
-	*/
-	
-	
+	 */
+
+
 	void tearDown() {
 		person = null;
 		System.gc();
@@ -183,6 +207,6 @@ class SalesPersonUnitTest {
 
 	@AfterAll
 	static void end() {
-		System.out.println("--------------- TEST ENDED -----------------");
+		System.out.println("--------------- SALESPERSON TEST ENDED -----------------");
 	}
 }
