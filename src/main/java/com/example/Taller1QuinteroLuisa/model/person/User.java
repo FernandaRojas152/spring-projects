4b6a4d88ae48cdabe1754d8192ae0a7c5410c56b
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-04-19 15:09:27 -0500

    enum added

---
+++ src/main/java/com/example/Taller1QuinteroLuisa/model/person/User.java
@@ -0,0 +1,5 @@
+package com.example.Taller1QuinteroLuisa.model.person;
+
+public class User {
+
+}
---
+++ src/main/java/com/example/Taller1QuinteroLuisa/model/person/UserType.java
@@ -0,0 +1,5 @@
+package com.example.Taller1QuinteroLuisa.model.person;
+
+public enum UserType {
+	administrador, operador
+}
