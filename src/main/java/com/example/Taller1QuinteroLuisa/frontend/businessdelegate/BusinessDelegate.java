b9257409db5cc9010dc25f8d71375f5aa0aa7308
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-06-04 18:53:18 -0500

    one test added but it's not working

--- src/main/java/com/example/Taller1QuinteroLuisa/frontend/businessdelegate/BusinessDelegate.java
+++ src/main/java/com/example/Taller1QuinteroLuisa/frontend/businessdelegate/BusinessDelegate.java
@@ -9,20 +9,12 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Currency;
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Currencyrate;
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Salesperson;
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Salespersonquotahistory;
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Salesterritory;
 import com.example.Taller1QuinteroLuisa.backend.model.sales.Salesterritoryhistory;
-import com.example.Taller1QuinteroLuisa.backend.restController.CurrencyRateRestController;
-import com.example.Taller1QuinteroLuisa.backend.restController.CurrencyRestController;
-import com.example.Taller1QuinteroLuisa.backend.restController.SalespersonQuotaRestController;
-import com.example.Taller1QuinteroLuisa.backend.restController.SalespersonRestController;
-import com.example.Taller1QuinteroLuisa.backend.restController.SalesterritoryHistoryRestController;
-import com.example.Taller1QuinteroLuisa.backend.restController.SalesterritoryRestController;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,13 +23,6 @@ public class BusinessDelegate {
 	@Getter
 	@Setter
 	private RestTemplate restTemplate;
-	
-	private SalespersonRestController personRest;
-	private SalesterritoryRestController territoryRest;
-	private SalespersonQuotaRestController personquotaRest;
-	private SalesterritoryHistoryRestController territoryHistoryRest;
-	private CurrencyRestController currencyRest;
-	private CurrencyRateRestController currencyrateRest;
 	
 	
 	private final String URLPERSON= "http://localhost:8080/api/salespersonRest/list";
--- src/test/java/com/example/Taller1/BusinessDelegateTest/BusinessDelegateTest.java
+++ src/test/java/com/example/Taller1/BusinessDelegateTest/BusinessDelegateTest.java
@@ -1,9 +1,15 @@
 package com.example.Taller1.BusinessDelegateTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Iterator;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,6 +24,12 @@ import com.example.Taller1QuinteroLuisa.frontend.businessdelegate.BusinessDelega
 @ContextConfiguration(classes= Taller1QuinteroLuisaApplication.class)
 @ExtendWith(SpringExtension.class)
 public class BusinessDelegateTest {
+	private final String URLPERSON= "http://localhost:8080/api/salespersonRest/list";
+	private final String URLPERSONQUOTA= "http://localhost:8080/api/salespersonquotaRest/list";
+	private final String URLTERRITORY= "http://localhost:8080/api/salesterritoryRest/list";
+	private final String URLTERRITORYHISTORY= "http://localhost:8080/api/salesterritoryhistoryRest/list";
+	private final String URLCURRENCY= "http://localhost:8080/api/salespersonRest/list";
+	private final String URLCURRENCYRATE= "http://localhost:8080/api/salespersonRest/list";
 	
 	@Mock
 	private RestTemplate restTemplate;
@@ -35,7 +47,21 @@ public class BusinessDelegateTest {
 	class Salesperson{
 		@BeforeEach
 		void setUp() {
+			delegate= new BusinessDelegate();
+			delegate.setRestTemplate(restTemplate);
+		}
+		
+		@Test
+		void findAllSalesPerson() {
+			Salesperson[] personList= new Salesperson[4];
+			for (int i = 0; i < personList.length; i++) {
+				Salesperson person= new Salesperson();
+				personList[i]= person;
+			}
 			
+			when(restTemplate.getForObject(URLPERSON,Salesperson[].class)).thenReturn(personList);
+			
+			assertEquals(delegate.getSalesPerson().size(),4);
 		}
 		
 	}
