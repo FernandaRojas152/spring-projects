679023d0bd73d332a1455f261d98e8de8366516c
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-06-04 22:49:21 -0500

    test fixed

--- src/test/java/com/example/Taller1/BusinessDelegateTest/BusinessDelegateTest.java
+++ src/test/java/com/example/Taller1/BusinessDelegateTest/BusinessDelegateTest.java
@@ -3,8 +3,8 @@ package com.example.Taller1.BusinessDelegateTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.Iterator;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,16 +13,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 import com.example.Taller1QuinteroLuisa.Taller1QuinteroLuisaApplication;
 import com.example.Taller1QuinteroLuisa.frontend.businessdelegate.BusinessDelegate;
+import com.example.Taller1QuinteroLuisa.backend.model.sales.Salesperson;
 
 @SpringBootTest
 @ContextConfiguration(classes= Taller1QuinteroLuisaApplication.class)
-@ExtendWith(SpringExtension.class)
 public class BusinessDelegateTest {
 	private final String URLPERSON= "http://localhost:8080/api/salespersonRest/list";
 	private final String URLPERSONQUOTA= "http://localhost:8080/api/salespersonquotaRest/list";
@@ -33,8 +34,10 @@ public class BusinessDelegateTest {
 	
 	@Mock
 	private RestTemplate restTemplate;
+	
 	@InjectMocks
-	private BusinessDelegate delegate;
+	BusinessDelegate delegate;
+	Salesperson person;
 	
 	@BeforeAll
 	static void init() {
@@ -44,11 +47,18 @@ public class BusinessDelegateTest {
 	
 	@Nested
 	@DisplayName("Test methods for sales person")
-	class Salesperson{
+	class SalespersonTests{
 		@BeforeEach
 		void setUp() {
+			MockitoAnnotations.initMocks(this);
+			person= new Salesperson();
+			person.setBusinessentityid(1);
+			person.setCommissionpct(new BigDecimal(1));
+			person.setSalesquota(new BigDecimal(30));
+			
 			delegate= new BusinessDelegate();
 			delegate.setRestTemplate(restTemplate);
+			delegate.addSalesperson(person);
 		}
 		
 		@Test
@@ -57,6 +67,7 @@ public class BusinessDelegateTest {
 			for (int i = 0; i < personList.length; i++) {
 				Salesperson person= new Salesperson();
 				personList[i]= person;
+				delegate.addSalesperson(person);
 			}
 			
 			when(restTemplate.getForObject(URLPERSON,Salesperson[].class)).thenReturn(personList);
